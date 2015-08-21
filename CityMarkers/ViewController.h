d82cec4f74717614a576fe2427118cbd3eb9b28c
Vita V <dj.vita.09@gmail.com>
2015-08-20 19:09:00 -0500

    Initial Commit

---
+++ CityMarkers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CityMarkers
+//
+//  Created by Elias Vltaliano Vidaurre Davila on 8/20/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CityMarkers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CityMarkers
+//
+//  Created by Elias Vltaliano Vidaurre Davila on 8/20/15.
+//  Copyright (c) 2015 vita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
